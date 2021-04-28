16aaac15727ec8d65ab5095a68a6566702d828e0
anjalivats2712 <77090512+anjalivats2712@users.noreply.github.com>
2021-04-28 15:20:46 +0530

    commit

---
+++ 1first.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+
+int main(){
+    printf("hello world");//this is another function printf in main function
+    return 0;
+}
+
+
---
+++ 3revision.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+int main(){
+    int a = 3;
+    float b =6.7;
+    char c='g';
+    printf("hello this is the scnd day \n");
+    printf("the value of a is %d", a);
+    return 0;
+}
