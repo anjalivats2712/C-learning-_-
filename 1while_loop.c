842cf2c5de5e7546a9c9121665877e411f8d6290
anjalivats2712 <77090512+anjalivats2712@users.noreply.github.com>
2021-05-06 21:28:04 +0530

    commit

---
+++ 1while_loop.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// int main(){
+//     int a;
+//     printf("enter the number\n");
+//     scanf("%d",&a);
+//     while(a<10){
+//         printf("the number is %d\n",a);
+//         a++;
+//     }
+//     return 0;
+// }
+
+int main(){
+    int b =0;
+    while(b<=20 && b>=10){
+        printf("%d",b);
+        b++;
+    }
+    printf("ye execute kyu nhi hua");
+    return 0;
+
+    
+}
