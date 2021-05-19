54cfce3d3fb84ca1d49f1c9a03e40bdc1eafbb99
anjalivats2712 <77090512+anjalivats2712@users.noreply.github.com>
2021-05-19 17:20:36 +0530

    commit

--- number_guessing.c
+++ number_guessing.c
@@ -1,27 +1,51 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <time.h>
+
+// int main(){
+//     int number,guess,nguesses=1;
+//     srand(time(0));
+//     number=rand()%100 +1;
+//     do{
+//         printf("guess the number btw 1 and 100\n");
+//         scanf("%d",&guess);
+//         if(guess>number){
+//             printf("lower number plz\n");
+//         }
+//         else if(guess<number){
+//             printf("higher number plz\n");
+//         }
+//         else{
+//             printf("the number of guesses is %d\n",nguesses);
+//         }
+//         nguesses++;
+//     }while(guess!=number);
+//     return 0;
+
+
+
+// }
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int main(){
-    int number,guess,nguesses=1;
+int main (){
+    int number, guess,nguesses =1;
     srand(time(0));
     number=rand()%100 +1;
     do{
-        printf("guess the number btw 1 and 100\n");
+        printf("you have to guess a number\n");
         scanf("%d",&guess);
-        if(guess>number){
+        if (number<guess){
             printf("lower number plz\n");
         }
-        else if(guess<number){
+        else if(number>guess){
             printf("higher number plz\n");
         }
         else{
-            printf("the number of guesses is %d\n",nguesses);
+            printf("you guessed for %d times before actually guessing it right\n",nguesses);
         }
         nguesses++;
-    }while(guess!=number);
+    }while(number!=guess);
     return 0;
-
-
-
 }
