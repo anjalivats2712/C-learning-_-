b01f28503c3c9bc8d4c28c59f38263b2bfa2b176
anjalivats2712 <77090512+anjalivats2712@users.noreply.github.com>
2021-05-20 15:08:55 +0530

    commit

---
+++ chapter 7/arrays_initialisation.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int marks[5]={12,23,45,34,56};
+    printf("the marks of student 1 is %d\n",marks[0]);
+    printf("the marks of student 2 is %d\n",marks[1]);
+    printf("the marks of student 3 is %d\n",marks[2]);
+    printf("the marks of student 4 is %d\n",marks[3]);
+    printf("the marks of student 5 is %d\n",marks[4]);
+    return 0;
+}
