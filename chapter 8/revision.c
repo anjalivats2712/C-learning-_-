29f6970236b1110be9adf34f80778907191a064a
anjalivats2712 <77090512+anjalivats2712@users.noreply.github.com>
2021-05-26 13:35:33 +0530

    commit

---
+++ chapter 8/revision.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    //initialising strings
+    char str[]={'a','n','j','a','l','i','\0'};
+    char stri[]="anjali vats";
+    char*ptr=str;
+    //printing strings
+    while(*ptr!='\0'){
+        printf("%c",*ptr);
+        ptr++;
+    }
+    //we have another method too 
+    char*pttr="stories";
+    printf("\n%s\n",pttr);
+    //taking inputs
+    char s[34];
+    printf("enter the name of season\n");
+    scanf("%s",s);//scanf cant take multi words strings with spaces 
+    printf("%s",s);
+    return 0;
+
+}
