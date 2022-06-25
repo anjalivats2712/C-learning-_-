731e266aee169d13f037908f739048b376a8ebf0
anjalivats2712 <77090512+anjalivats2712@users.noreply.github.com>
2022-06-25 09:53:34 +0530

    commit

---
+++ c++/revision/basic.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"hello world"<<endl; // << insertion operator
+    cout<<"enter the value of a\n";
+    cin>>a;//>> extraction operator
+    //string 
+    string str,str2;
+    str="this is a tree this is not a tree\n";
+    cout<<str;
+    cin>>str2;
+    cout<<str2;
+
+
+
+    
+    return 0;
+}
